bd96b7acee4c3b09a9b11702662a76447ec56f18
Gregory Rose <greg@ncroses.com>
2015-02-06 00:00:29 -0500

    drag onto board, basic row-col validation only

---
+++ 1_sudoku/cluedoku/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class BoardManager : MonoBehaviour {
+
+	const float SIZE = 2.5f;
+
+	private int AdjustToInt(float f) {
+		return Mathf.RoundToInt(.5f + f / SIZE) + 1;
+	}
+	
+	private int Classify(GameObject g) {
+		string n = g.transform.parent.name;
+		if (n == "Suspects")
+			return 0;
+		else if (n == "Locations")
+			return 1;
+		else if (n == "Victims")
+			return 2;
+		else if (n == "Weapons")
+			return 3;
+		
+		Debug.LogError("unknown " + n);
+		return 4;
+	}
+
+	bool CheckBoard() {
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+		GameObject[,] board = new GameObject[4, 4];
+		
+		foreach (GameObject tile in tiles) {
+			Vector3 pos = tile.transform.position;
+			int x = AdjustToInt(pos.x);
+			int y = AdjustToInt(pos.y);
+			if (x < 0 || x > 3 || y < 0 || y > 3) {
+				continue;
+			}
+			
+			if (board[x,y] != null) {
+				Debug.Log ("duplicate tile");
+				return false;
+			}
+			board[x,y] = tile;
+		}
+		
+		for (int x = 0; x < 4; x++) {
+			int[] test = new int[4];
+			int[] test2 = new int[4];
+			
+			for (int y = 0; y < 4; y++) {
+				if (board[x,y] == null || board[y,x] == null)
+					return false;
+					
+				test[Classify(board[x,y])] += 1;
+				test2[Classify(board[y,x])] += 1;
+			}
+			
+			if (test[0] != 1 || test[1] != 1 || test[2] != 1 || test[3] != 1) {
+				Debug.Log ("Fail col " + x);
+				Debug.Log (string.Join(",", test.Select(k => k.ToString()).ToArray()));
+				return false;
+			}
+			
+			if (test2[0] != 1 || test2[1] != 1 || test2[2] != 1 || test2[3] != 1) {
+				Debug.Log ("Fail row " + x);
+				Debug.Log (string.Join(",", test2.Select(k => k.ToString()).ToArray()));
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp(0)) {
+			Debug.Log (CheckBoard());
+		}
+	}
+	
+}
---
+++ 1_sudoku/cluedoku/Assets/Scripts/Drag.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+// from https://stackoverflow.com/questions/23152525/drag-object-in-unity-2d
+public class Drag : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private bool wasDragged;
+	const float SIZE = 2.5f;
+	
+	void Start() {
+		Snap ();
+	}
+	
+	void OnMouseDown() {
+		
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+		wasDragged = true;
+	}
+	
+	private float Adjust(float f) {
+		return (Mathf.Round(.5f + f / SIZE) - .5f) * SIZE;
+	}
+	
+	private void Snap() {
+		Vector3 pos = transform.position;
+		pos.x = Adjust(pos.x);
+		pos.y = Adjust(pos.y);
+		transform.position = pos;
+	}
+	
+	void OnMouseUp() {
+		if (wasDragged) {
+			wasDragged = false;
+			Snap();
+		}
+	}
+}
