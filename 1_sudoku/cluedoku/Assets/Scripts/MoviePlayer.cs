3a663f49e3c7bb99e8d9410615466129c6d04af8
Gregory Rose <greg@ncroses.com>
2015-02-10 18:56:57 -0500

    show video

---
+++ 1_sudoku/cluedoku/Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = movTexture;
+		movTexture.Play();
+		Debug.Log (movTexture.duration);
+		StartCoroutine(AdvanceToNextLevelAfter(movTexture.duration));
+	}
+	
+	IEnumerator AdvanceToNextLevelAfter(float seconds) {
+		yield return new WaitForSeconds(seconds);
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
