cb0929fa1c31ff62990c0840c5b87347922d5ca6
itbmac <itbmac@yahoo.com>
2015-02-11 18:22:48 -0500

    Instruction Screen

---
+++ 1_sudoku/cluedoku/Assets/ClickToDisable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToDisable : MonoBehaviour {
+
+	public GameObject followingItem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (renderer.enabled && Input.GetMouseButtonDown(0)) {
+			if (followingItem)
+				followingItem.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
