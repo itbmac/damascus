66fccefc766cbf2b5c50e14b1d9a06f73aa5a4a0
itbmac <itbmac@yahoo.com>
2015-02-11 19:33:31 -0500

    Added Instructions Open Icon to Game

--- 1_sudoku/cluedoku/Assets/ClickToDisable.cs
+++ 1_sudoku/cluedoku/Assets/ClickToDisable.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class ClickToDisable : MonoBehaviour {
 
-	public GameObject followingItem;
+	public GameObject followingItem, additionalObjDisableOnClick;
 	public AudioClip AdvanceNoise;
+	public bool disableRendererOnClick = true, playSoundOnVisible = false;
+	private bool prevRendererState = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,9 +17,26 @@ public class ClickToDisable : MonoBehaviour {
 	void Update () {
 		if (renderer.enabled && Input.GetMouseButtonDown(0)) {
 			audio.PlayOneShot(AdvanceNoise);
-			if (followingItem)
+			if (followingItem) {
 				followingItem.SetActive(true);
-			renderer.enabled = false;
+				if (followingItem.renderer)
+					followingItem.renderer.enabled = true;
+			}
+
+			if (disableRendererOnClick)
+				renderer.enabled = false;
+
+			if (additionalObjDisableOnClick)
+				additionalObjDisableOnClick.SetActive(false);
 		}
+		else if (renderer.enabled) {
+			followingItem.SetActive(false);
+		}
+
+		if (!prevRendererState && renderer.enabled) {
+			audio.PlayOneShot(AdvanceNoise);
+		}
+
+		prevRendererState = renderer.enabled;
 	}
 }
