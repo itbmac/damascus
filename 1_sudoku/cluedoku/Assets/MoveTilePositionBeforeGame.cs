1dceb3ce24afa1d6dc7650ce87c17d7e33ff4eff
itbmac <itbmac@yahoo.com>
2015-02-20 05:59:58 -0500

    SO TIRED. MUCH HACK. VERY LAZY :D

---
+++ 1_sudoku/cluedoku/Assets/MoveTilePositionBeforeGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTilePositionBeforeGame : MonoBehaviour {
+
+	public GameObject verifyOkayToMoveOffOfObj;
+	public Vector2 desiredTilePlacement;
+	public Vector2 desiredTilePlacementLoc;
+	public bool hasMoved = false;
+
+	// Use this for initialization
+	void Start () {
+		desiredTilePlacementLoc = BoardManager.Instance.GridCoord2Pos(new GridCoord((int)desiredTilePlacement.x, (int)desiredTilePlacement.y));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (verifyOkayToMoveOffOfObj && verifyOkayToMoveOffOfObj.activeSelf) {
+			gameObject.GetComponent<TileController>().Move(desiredTilePlacementLoc);
+			hasMoved = true;
+			this.enabled = false;
+		}
+	}
+}
