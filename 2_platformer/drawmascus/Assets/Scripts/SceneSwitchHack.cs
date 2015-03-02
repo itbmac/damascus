0bfde6d82ec285604b313891149eb3c6f652d3d2
Sarah Chen <swchen@andrew.cmu.edu>
2015-03-02 00:38:11 -0500

    Basic Scene Layout
    Made separate scenes for different parts of the game. Hopefully this
    will make editing levels a little easier on the programmers.

---
+++ 2_platformer/drawmascus/Assets/Scripts/SceneSwitchHack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitchHack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("0"))
+			Application.LoadLevel (0);
+		if (Input.GetKeyDown("1"))
+			Application.LoadLevel (1);
+		if (Input.GetKeyDown("2"))
+			Application.LoadLevel (2);
+		if (Input.GetKeyDown("3"))
+			Application.LoadLevel (3);
+	}
+}
