a339eaa03eebc51995f9a14520a5d04e4dadf99d
Gregory Rose <greg@ncroses.com>
2015-03-18 18:17:32 -0400

    Bird, feather

---
+++ 2_platformer/drawmascus/Assets/Scripts/ColorToggle/Bird.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ColorToggle))]
+public class Bird : MonoBehaviour, Colorable {
+
+	public GameObject Feather;
+	public float SnakeRadius = 2.0f;
+	public float EscapeDistance = 10.0f;
+	public float EscapeVelocity = 5.0f;
+
+	private GameObject snake;
+	
+	private bool scared;
+	private Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		snake = FindObjectOfType<Snake>().gameObject;
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!scared) {
+			if (Vector2.Distance(snake.transform.position, transform.position) < SnakeRadius) {
+				scared = true;
+				Instantiate(Feather, transform.position, Quaternion.identity);
+			}
+		} else {
+			if (Vector2.Distance(startPos, transform.position) > EscapeDistance) {
+				Destroy(gameObject);
+			} else {
+				transform.position += new Vector3(1, 1, 0) * EscapeVelocity * Time.deltaTime;
+			}
+		}
+	}
+	
+	public void OnTurnReal() {
+	
+	}
+	
+	public void OnTurnDrawing() {
+	
+	}
+}
