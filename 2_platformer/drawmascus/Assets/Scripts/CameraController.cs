9336d63029ee75057f09b2befb6d278e6aa979bc
Gregory Rose <greg@ncroses.com>
2015-03-19 00:02:26 -0400

    Camera bounds

--- 2_platformer/drawmascus/Assets/Scripts/CameraController.cs
+++ 2_platformer/drawmascus/Assets/Scripts/CameraController.cs
@@ -7,20 +7,40 @@ public class CameraController : MonoBehaviour {
 	public float DampTime = 0.15f;
 	private Vector3 velocity = Vector3.zero;
 	
+	private GameObject CameraMin;
+	private GameObject CameraMax;
+	
 	Vector3 GetDestination() {		
 		Transform target = Target.transform;
 		Vector3 t_pos = target.position;
+		
+		
 		//t_pos.y += 0.5f * scale;
 		Vector3 point = camera.WorldToViewportPoint(t_pos);
 		Vector3 delta = t_pos - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 		Vector3 destination = transform.position + delta;
+		
+		
+		Vector3 extents = camera.ViewportToWorldPoint(Vector3.one) - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+		if (CameraMax != null) {
+			destination = Vector3.Min(destination + extents, CameraMax.transform.position) - extents;
+		}
+		
+		if (CameraMin != null) {
+			destination = Vector3.Max(destination - extents, CameraMin.transform.position) + extents;
+		}
+		
+		destination.z = -10;
 		
 		return destination;
 	}
 
 	// Use this for initialization
 	void Start () {
-		Target = FindObjectOfType<Player>().gameObject;
+		Target = FindObjectOfType<Player>().gameObject;		
+		CameraMin = GameObject.Find("CameraMin");
+		CameraMax = GameObject.Find("CameraMax");
+		
 		transform.position = GetDestination();
 	}
 	
---
+++ 2_platformer/drawmascus/Assets/Scripts/HideSpriteOnStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideSpriteOnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = false;
+	}
+}
