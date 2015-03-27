607cf67bf167bcbb3cc0cc63efd913952fa0af32
Sarah Chen <swchen@andrew.cmu.edu>
2015-03-27 00:12:24 -0400

    Start Menu
    god it looks cool

--- 2_platformer/drawmascus/Assets/Scripts/LoadLevelButton.cs
+++ 2_platformer/drawmascus/Assets/Scripts/LoadLevelButton.cs
@@ -7,6 +7,16 @@ public class LoadLevelButton : MonoBehaviour {
 	public string TargetLevelName;
 
 	void OnMouseDown() {
+		if (TargetLevelName == "Quit")
+			Application.Quit();
 		Application.LoadLevel(TargetLevelName);
+	}
+
+	void OnMouseEnter() {
+		transform.parent.renderer.enabled = true;
+	}
+	
+	void OnMouseExit() {
+		transform.parent.renderer.enabled = false;
 	}
 }
