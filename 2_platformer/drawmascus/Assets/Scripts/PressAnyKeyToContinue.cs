9c038eeb6b578c1dd2a4916cd52eac2f1bb53b37
Gregory Rose <greg@ncroses.com>
2015-03-27 00:34:01 -0400

    Fix stuff

--- 2_platformer/drawmascus/Assets/Scripts/PressAnyKeyToContinue.cs
+++ 2_platformer/drawmascus/Assets/Scripts/PressAnyKeyToContinue.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class PressAnyKeyToContinue : MonoBehaviour {
+
+	public string DestLevel;
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			if (DestLevel != "") {
+				Application.LoadLevel(DestLevel);
+				return;
+			}			
+		
 			if (Application.loadedLevel == Application.levelCount - 1)
 				Application.LoadLevel(0);
 			else
