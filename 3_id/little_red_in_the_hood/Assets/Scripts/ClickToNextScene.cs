43e36480cfa704151c139bead2587348259c2394
itbmac <itbmac@yahoo.com>
2015-03-28 00:23:14 -0400

    Intro Video Added

---
+++ 3_id/little_red_in_the_hood/Assets/Scripts/ClickToNextScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+			Application.LoadLevel(Application.loadedLevel + 1);
+	
+	}
+}
---
+++ 3_id/little_red_in_the_hood/Assets/Scripts/MoviePlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	public float numSecondsToEndEarly = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.Play();
+		GetComponent<AudioSource>().Play();
+		Debug.Log (movTexture.duration);
+		StartCoroutine(AdvanceToNextLevelAfter(movTexture.duration));
+	}
+	
+	IEnumerator AdvanceToNextLevelAfter(float seconds) {
+		yield return new WaitForSeconds(seconds - numSecondsToEndEarly);
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
