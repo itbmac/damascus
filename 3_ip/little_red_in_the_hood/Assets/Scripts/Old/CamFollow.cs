c1b7e9d7f464c1dfbceab7befa90f13686756959
itbmac <itbmac@yahoo.com>
2015-04-03 10:54:53 -0400

    DEMO 2

--- 3_ip/little_red_in_the_hood/Assets/Scripts/Old/CamFollow.cs
+++ 3_ip/little_red_in_the_hood/Assets/Scripts/Old/CamFollow.cs
@@ -3,18 +3,62 @@ using System.Collections;
 
 public class CamFollow : MonoBehaviour {
 	public float cameraSize = 12.0f;
+	public float cameraSizeTarget = 12.0f;
+	public float cameraSizeStart = 8.0f;
+	public float cameraSizeZoomed = 12.0f;
+	public float cameraSizeTransitionTimeStandard = 1.3f;
+	public float cameraSizeTransitionTimeFactorCurrent = 1.0f;
+	private float cameraSizeTransitionTimeStart;
+
+	private Vector3 originPosition;
+	private Quaternion originRotation;
+	public float shake_decay = 0.002f;
+	public float shake_intensity = 0f;
+	public float SHAKE_INTENSITY_0 = 0.015f;
+	public float SHAKE_INTENSITY_1 = 0.025f;
+	public float SHAKE_INTENSITY_2 = 0.035f;
+	public float SHAKE_INTENSITY_3 = 0.050f;
+	public float SHAKE_INTENSITY_4 = 0.075f;
+	private Camera cam;
+
 	private Vector3 velocity = Vector3.zero;
 	public Transform target;
 
 	void Start() {
-		Camera cam = this.GetComponent<Camera>();
+		cam = this.GetComponent<Camera>();
 		cameraSize = cam.orthographicSize;
+		cameraSizeTarget = cameraSize;
+		cameraSize = cameraSizeStart;
+		shake_intensity = 0.0f;
 	}
 
 	void Update()
 	{
 		if (target)
 		{
+
+			if (cameraSize != cameraSizeTarget) {
+				if (cameraSize < cameraSizeTarget) {
+					cameraSize = Mathf.Min(cameraSizeTarget, cameraSize + (Time.time - cameraSizeTransitionTimeStart)/(cameraSizeTransitionTimeStandard * cameraSizeTransitionTimeFactorCurrent));
+				}
+				else {
+					cameraSize = Mathf.Max(cameraSizeTarget, cameraSize - (Time.time - cameraSizeTransitionTimeStart)/(cameraSizeTransitionTimeStandard * cameraSizeTransitionTimeFactorCurrent));
+				}
+				cam.orthographicSize = cameraSize;
+			}
+
+			if (shake_intensity > 0) {
+				Vector3 shake = Random.insideUnitSphere * shake_intensity;
+				shake.z = 0.0f;
+				transform.position = originPosition + shake;
+				transform.rotation = new Quaternion(
+					originRotation.x + Random.Range (-shake_intensity,shake_intensity) * .2f,
+					originRotation.y + Random.Range (-shake_intensity,shake_intensity) * .2f,
+					originRotation.z, // + Random.Range (-shake_intensity,shake_intensity) * .2f,
+					originRotation.w); // + Random.Range (-shake_intensity,shake_intensity) * .2f);
+				shake_intensity -= shake_decay;
+			}
+
 			float player_x = target.position.x;
 			float player_y = target.position.y;
 
@@ -33,5 +77,18 @@ public class CamFollow : MonoBehaviour {
 		valueInPixels = Mathf.Round(valueInPixels);
 		float roundedUnityUnits = valueInPixels * (1 / pixelToUnits);
 		return roundedUnityUnits;
+	}
+
+	public void ChangeSize(float newCameraSize, float transitionSpeed = 1.0f) {
+		cameraSizeTransitionTimeStart = Time.time;
+		cameraSizeTarget = newCameraSize;
+		cameraSizeTransitionTimeFactorCurrent = transitionSpeed;
+	}
+	
+	public void Shake(float shakeIntensity, float shakeDecay = 0.002f){
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+		shake_intensity = shakeIntensity;
+		shake_decay = shakeDecay;
 	}
 }
--- 3_ip/little_red_in_the_hood/Assets/TheGameManager.cs
+++ 3_ip/little_red_in_the_hood/Assets/TheGameManager.cs
@@ -11,6 +11,7 @@ public class TheGameManager : MonoBehaviour {
 	
 	public float DetectionTime = 2.0f;	
 	private float DetectedStartTime = -3f;
+	public bool DetectedCurrently = false;
 	
 	public static TheGameManager Instance { get; private set; }
 	
@@ -26,10 +27,18 @@ public class TheGameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (DetectedCurrently && (DetectedTimeRemaining <= 0)) {
+			Camera.main.GetComponent<CamFollow>().ChangeSize(Camera.main.GetComponent<CamFollow>().cameraSizeStart);
+			DetectedCurrently = false;
+		}
 	}
 	
 	public void Detected() {
 		DetectedStartTime = Time.time;
+		Camera.main.GetComponent<CamFollow>().ChangeSize(Camera.main.GetComponent<CamFollow>().cameraSizeZoomed);
+
+		if (!DetectedCurrently)
+			Camera.main.GetComponent<CamFollow>().Shake(Camera.main.GetComponent<CamFollow>().SHAKE_INTENSITY_4);
+		DetectedCurrently = true;
 	}
 }
