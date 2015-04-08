81060f9744826f75f62a7317304184e331c52c69
Gregory Rose <greg@ncroses.com>
2015-04-08 17:18:07 -0400

    Experiment complete.

--- 3_ip/little_red_in_the_hood/Assets/ExperimentSprite.cs
+++ 3_ip/little_red_in_the_hood/Assets/ExperimentSprite.cs
@@ -3,22 +3,15 @@ using System.Collections;
 
 public class ExperimentSprite : MyMonoBehaviour {
 
+	public float UpdateTime = 1.0f;
+
+	Texture2D originalTexture;
+	Texture2D texture ;
+
 	// Use this for initialization
 	void Start () {	
-		var texture = Instantiate(spriteRenderer.sprite.texture);
-		
-		var pixels = texture.GetPixels();
-		
-		for (int i = 0; i < texture.height; i++) {
-			if (i % 10 < 5) continue;
-			for (int j = 0; j < texture.width; j++) {
-				pixels[i * texture.width + j] = Color.black;
-			}
-		}
-		
-		texture.SetPixels(pixels);
-		texture.Apply();	
-	
+		originalTexture = spriteRenderer.sprite.texture;
+		texture = Instantiate(spriteRenderer.sprite.texture);
 		spriteRenderer.sprite = Sprite.Create(
 			texture,
 			new Rect(0, 0, texture.width, texture.height),
@@ -26,8 +19,33 @@ public class ExperimentSprite : MyMonoBehaviour {
 		);
 	}
 	
+	float nextUpdate;
+	int min;
+	
 	// Update is called once per frame
 	void Update () {
+		if (Time.time < nextUpdate)
+			return;
+		else
+			nextUpdate = Time.time + UpdateTime;
 	
+		var pixels = originalTexture.GetPixels();
+		
+		
+		int max = min + 10;
+		
+		for (int i = 0; i < texture.height; i++) {
+			int m = i % 50;
+			if (!(min <= m && m <= max)) continue;
+			
+			for (int j = 0; j < texture.width; j++) {
+				pixels[i * texture.width + j] = Color.black;
+			}
+		}
+		
+		texture.SetPixels(pixels);
+		texture.Apply();
+		
+		min = (min + 10) % 50;
 	}
 }
