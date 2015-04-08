05ec18358dcb471a89453da2ceef3fc5df013c80
Gregory Rose <greg@ncroses.com>
2015-04-08 17:11:08 -0400

    WIP. Sprite changing.

---
+++ 3_ip/little_red_in_the_hood/Assets/ExperimentSprite.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExperimentSprite : MyMonoBehaviour {
+
+	// Use this for initialization
+	void Start () {	
+		var texture = Instantiate(spriteRenderer.sprite.texture);
+		
+		var pixels = texture.GetPixels();
+		
+		for (int i = 0; i < texture.height; i++) {
+			if (i % 10 < 5) continue;
+			for (int j = 0; j < texture.width; j++) {
+				pixels[i * texture.width + j] = Color.black;
+			}
+		}
+		
+		texture.SetPixels(pixels);
+		texture.Apply();	
+	
+		spriteRenderer.sprite = Sprite.Create(
+			texture,
+			new Rect(0, 0, texture.width, texture.height),
+			new Vector2(0.5f,0.5f)
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 3_ip/little_red_in_the_hood/Assets/Scripts/VisionAdapter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionAdapter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float width = 5;
+		float height = 5;
+	
+		Mesh m = new Mesh();
+		m.name = "ScriptedMesh";
+		m.vertices = new Vector3[] {
+			new Vector3(-width, -height, 0.01f),
+			new Vector3(width, -height, 0.01f),
+			new Vector3(width, height, 0.01f),
+			new Vector3(-width, height, 0.01f)
+		};
+		m.uv = new Vector2[] {
+			new Vector2 (0, 0),
+			new Vector2 (0, 1),
+			new Vector2(1, 1),
+			new Vector2 (1, 0)
+		};
+		m.triangles = new int[] { 0, 1, 2, 0, 2, 3};
+		m.normals = new Vector3[] {
+			new Vector3(0, 0, -1),
+			new Vector3(0, 0, -1),
+			new Vector3(0, 0, -1),
+			new Vector3(0, 0, -1)
+		};
+		
+		GetComponent<MeshFilter>().mesh = m;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
