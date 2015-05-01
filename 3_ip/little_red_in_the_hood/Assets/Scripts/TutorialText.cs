912e4395a11cbf7990c077f4d7b836b278f95611
Gregory Rose <greg@ncroses.com>
2015-04-30 21:48:18 -0400

    Tutorial text

---
+++ 3_ip/little_red_in_the_hood/Assets/Scripts/TutorialText.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour {
+
+	public static TutorialText Instance {
+		get; private set;
+	}
+	
+	Text myText;
+	
+	void Awake() {
+		Instance = this;	
+	}
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text>();
+		myText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void TriggerText(string text) {
+		myText.text = text ?? "";
+	}
+}
---
+++ 3_ip/little_red_in_the_hood/Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialTrigger : MyMonoBehaviour {
+
+	public string TextToTrigger = "";
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Player") {
+			TutorialText.Instance.TriggerText(TextToTrigger);
+		}
+	}
+}
